b5457bd58f3840d4b9cde7084aec1004d558f8cc
Supa <36031171+0Supa@users.noreply.github.com>
2024-11-08 21:57:45 +0200

    + discord stats

--- cmd/ping.go
+++ cmd/ping.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"runtime"
 
+	discordClient "github.com/0supa/whatbot/client/discord"
 	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/api/cmdroute"
 )
@@ -22,8 +23,25 @@ func init() {
 
 			res := "# <a:gopherDance:1203488020389437480> Pong!\n" +
 				CodeBlock("md",
-					fmt.Sprintf("# %s\n- TotalAlloc: %v MiB\n- NumGC: %v\n- NumGoroutine: %v",
-						runtime.Version(), m.TotalAlloc/1024/1024, m.NumGC, runtime.NumGoroutine()))
+					fmt.Sprintf(
+						`
+# Discord
+- Gateway: %v ms
+- Guilds: %v
+# %s
+- Alloc: %v MiB
+- NumGC: %v
+- NumGoroutine: %v`,
+						// Discord
+						discordClient.Handler.Gateway().Latency().Milliseconds(),
+						len(discordClient.Handler.Ready().Guilds),
+						// go
+						runtime.Version(),
+						m.Alloc/1024/1024,
+						m.NumGC,
+						runtime.NumGoroutine(),
+					),
+				)
 
 			return Response(res)
 		},
